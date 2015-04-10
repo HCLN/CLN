42ffafc3d90524bd0b7a379b83f077a795be22c0
pablo castarataro <pablocastarataro@gmail.com>
2015-04-10 14:37:54 -0300

    Initial Import

---
+++ CLN/CLN/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CLN
+//
+//  Created by Pablo Castarataro on 4/10/15.
+//  Copyright (c) 2015 TBH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder<UIApplicationDelegate>
+
+@property(strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CLN/CLN/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CLN
+//
+//  Created by Pablo Castarataro on 4/10/15.
+//  Copyright (c) 2015 TBH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
